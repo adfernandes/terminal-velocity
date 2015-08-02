b20ce9638e34d7fc8288ffec09f2fe4b66cc1cc3
Andrew Fernandes <andrew@fernandes.org>
2015-08-02 17:30:26 -0400

    Wire up the user interface

--- Terminal Velocity/ViewController.swift
+++ Terminal Velocity/ViewController.swift
@@ -11,15 +11,33 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBOutlet weak var radio: NSMatrix!
+
     let defaults = NSUserDefaults(suiteName: "group.Y629ETSHLM.com.pharynks.terminalvelocity")
     let launcherPath = NSBundle.mainBundle().pathForResource("Terminal Velocity Launcher", ofType: "app")
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Start here...
+        let buttonCell = radio.selectedCell() as! NSButtonCell
+        let tag = buttonCell.tag
+
+        // TODO: If there is no default, save the current tag as the default.
 
     }
 
+    @IBAction func install(sender: NSButton) {
+
+        println("install") // TODO
+
+    }
+
+    @IBAction func preference(sender: NSMatrix) {
+        let buttonCell = sender.selectedCell() as! NSButtonCell
+        let tag = buttonCell.tag
+
+        println("preference: \(tag)") // TODO
+
+    }
 
 }
