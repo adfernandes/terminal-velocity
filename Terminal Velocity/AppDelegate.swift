f2755ddb8c040fd7a7f283dab1ab5ce9c4ec2bcf
Andrew Fernandes <andrew@fernandes.org>
2015-08-02 16:48:38 -0400

    Programatically center the main window

--- Terminal Velocity/AppDelegate.swift
+++ Terminal Velocity/AppDelegate.swift
@@ -7,7 +7,13 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // this space is intentionally left blank
+
+        let window: NSWindow = NSApplication.sharedApplication().windows.first! as! NSWindow
+
+        window.center()
+        window.level = Int(CGWindowLevelForKey(Int32(kCGNormalWindowLevelKey)))
+        window.makeKeyAndOrderFront(nil)
+
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
