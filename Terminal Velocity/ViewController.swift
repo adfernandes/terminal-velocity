10fa6375049e631bb09a8d39a6f0071e3ad88c71
Andrew Fernandes <andrew@fernandes.org>
2015-08-02 20:24:22 -0400

    Add plumbing for xterm, use enums for radio buttons, add appcast directory

--- Terminal Velocity/ViewController.swift
+++ Terminal Velocity/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: NSViewController {
 
     @IBOutlet weak var radio: NSMatrix!
 
+    enum RadioButtonTag : Int {
+        case Terminal = 0
+        case iTerm = 1
+        case xterm = 2
+    }
+
     let defaults = NSUserDefaults(suiteName: "group.Y629ETSHLM.com.pharynks.terminalvelocity")
     let launcherPath = NSBundle.mainBundle().pathForResource("Terminal Velocity Launcher", ofType: "app")
 
@@ -20,7 +26,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         let buttonCell = radio.selectedCell() as! NSButtonCell
-        let tag = buttonCell.tag
+        let tag = RadioButtonTag(rawValue: buttonCell.tag)
 
         // TODO: If there is no default, save the current tag as the default.
 
@@ -34,9 +40,25 @@ class ViewController: NSViewController {
 
     @IBAction func preference(sender: NSMatrix) {
         let buttonCell = sender.selectedCell() as! NSButtonCell
-        let tag = buttonCell.tag
+        let tag = RadioButtonTag(rawValue: buttonCell.tag)
 
-        println("preference: \(tag)") // TODO
+        println("preference: \(tag)") // TODO - also, add the 'xterm' option
+
+        // xterm -e "cd '/Applications' && pwd && bash -l"
+        //
+        // NSTask *task = [[NSTask alloc] init];
+        // task.launchPath = @"/usr/bin/grep";
+        // task.arguments = @[@"foo", @"bar.txt"];
+        // [task launch];
+        //
+        // Or use:
+        //
+        // let task = NSTask.launchedTaskWithLaunchPath(_ path: String, arguments arguments: [NSString]) -> NSTask
+        // task.launch()
+        //
+        // The strings in arguments do not undergo shell expansion, so you do not need to do special quoting, and shell variables, such as $PWD, are not resolved.
+        //
+        // BETTER: Use the 'currentDirectoryPath' property prior to launch, rather than using the 'cd' command! Then 'xterm -e "pwd && bash -l"'
 
     }
 
