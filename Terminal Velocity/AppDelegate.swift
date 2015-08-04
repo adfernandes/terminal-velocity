ab9791b9dbf5c51e14e06d53e5b8bdc07df3d76f
Andrew Fernandes <andrew@fernandes.org>
2015-08-04 16:00:39 -0400

    Get ready for update checking

--- Terminal Velocity/AppDelegate.swift
+++ Terminal Velocity/AppDelegate.swift
@@ -2,26 +2,32 @@
 //  Copyright (c) 2015 Pharynks Corporation. All rights reserved.
 
 import Cocoa
+import Sparkle
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
 
-        let window: NSWindow = NSApplication.sharedApplication().windows.first! as! NSWindow
+        let updater = SUUpdater.sharedUpdater()
+        let window = NSApplication.sharedApplication().windows.first! as! NSWindow
+        let arguments = NSProcessInfo.processInfo().arguments as! [String]
+
+        println("lastUpdateCheckDate: \(updater.lastUpdateCheckDate)")
 
         window.center()
         window.level = Int(CGWindowLevelForKey(Int32(kCGNormalWindowLevelKey))) // or kCGStatusWindowLevelKey, but this hides the 'about' dialog
         window.makeKeyAndOrderFront(nil)
+        // TODO // window.orderOut(nil)
 
-    }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // this space is intentionally left blank
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
         return true
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // this space is intentionally left blank
     }
 
 }
